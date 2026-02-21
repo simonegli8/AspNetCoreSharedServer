7ffb7328e2951850a8752a22c47b123467d68c01
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2026-02-21 14:56:53 +0100

    Bugfix

--- CreatePackages.bat
+++ CreatePackages.bat
@@ -5,4 +5,4 @@ del src\AspNetCoreSharedServer\bin\%Configuration%\*.nupkg
 del src\AspNetCoreSharedServer.Api\bin\%Configuration%\*.nupkg
 del src\AspNetCoreSharedServer.Library\bin\%Configuration%\*.nupkg
 
-dotnet pack src\AspNetCoreSharedServer.sln -c %Configuration%
+dotnet pack src\AspNetCoreSharedServer.slnx -c %Configuration%
