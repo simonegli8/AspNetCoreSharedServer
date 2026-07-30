ea4544f411b60eb0c3d808bb4c496349ea1b1414
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2026-07-30 07:47:04 +0200

    Fix

--- CreatePackages.bat
+++ CreatePackages.bat
@@ -4,4 +4,16 @@ SET Configuration=Debug
 del nupkg\*.nupkg
 del nupkg\*.snupkg
 
-dotnet pack AspNetCoreSharedServer.slnx -c %Configuration% -p:Version=%PackageVersion% -p:FileVersion=%PackageVersion%.0 -p:AssemblyVersion=%PackageVersion%
+dotnet pack AspNetCoreSharedServer.slnx -c %Configuration% ^
+    -p:Version=%PackageVersion% ^
+    -p:FileVersion=%PackageVersion%.0 ^
+    -p:AssemblyVersion=%PackageVersion%
+
+dotnet pack AspNetCoreSharedServer.slnx -c %Configuration% ^
+    -p:Version=%PackageVersion% ^
+    -p:FileVersion=%PackageVersion%.0 ^
+    -p:AssemblyVersion=%PackageVersion% ^
+    -p:DebugType=portable ^
+    -p:DebugSymbols=true ^
+    -p:IncludeSymbols=true ^
+    -p:SymbolPackageFormat=snupkg
--- CreatePackages.sh
+++ CreatePackages.sh
@@ -11,3 +11,13 @@ dotnet pack AspNetCoreSharedServer.slnx \
     -p:Version="$PackageVersion" \
     -p:FileVersion="${PackageVersion}.0" \
     -p:AssemblyVersion="$PackageVersion"
+
+dotnet pack AspNetCoreSharedServer.slnx \
+    -c "$Configuration" \
+    -p:Version="$PackageVersion" \
+    -p:FileVersion="${PackageVersion}.0" \
+    -p:AssemblyVersion="$PackageVersion" \
+    -p:DebugType=portable \
+    -p:DebugSymbols=true \
+    -p:IncludeSymbols=true \
+    -p:SymbolPackageFormat=snupkg
